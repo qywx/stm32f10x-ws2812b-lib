15b5bdde7d54552e9680e74c54fa1afae6330fcd
qyw <i.kyb@ya.ru>
2016-04-09 15:56:20 +0300

    Configuration file for PA11, TIM1, and corresponding DMA channel. Not tested yet.

--- inc/ws2812b_conf.h
+++ inc/ws2812b_conf.h
@@ -33,30 +33,30 @@
 #define WS2812B_USE_GAMMA_CORRECTION
 #define WS2812B_USE_PRECALCULATED_GAMMA_TABLE
 
-#define WS2812B_BUFFER_SIZE     60
+#define WS2812B_BUFFER_SIZE     30//60
 #define WS2812B_START_SIZE      2
 
-#define WS2812B_APB1_RCC        RCC_APB1Periph_TIM4
-#define WS2812B_APB2_RCC        RCC_APB2Periph_GPIOB
+#define WS2812B_TIMx_RCC_ENABLE()  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE)  //#define WS2812B_APB1_RCC        RCC_APB1Periph_TIM4
+#define WS2812B_GPIO_RCC_ENABLE()  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE) //#define WS2812B_APB2_RCC        RCC_APB2Periph_GPIOB
 
-#define WS2812B_AHB_RCC         RCC_AHBPeriph_DMA1
+#define WS2812B_DMA_RCC_ENABLE()   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE); //#define WS2812B_AHB_RCC         RCC_AHBPeriph_DMA1
 
-#define WS2812B_GPIO            GPIOB
-#define WS2812B_GPIO_PIN        GPIO_Pin_6
+#define WS2812B_GPIO            GPIOA
+#define WS2812B_GPIO_PIN        GPIO_Pin_11
 
-#define WS2812B_TIM             TIM4
-#define WS2812B_TIM_OCINIT      TIM_OC1Init
-#define WS2812B_TIM_OCPRELOAD   TIM_OC1PreloadConfig
-#define WS2812B_TIM_DMA_CC      TIM_DMA_CC1
-#define WS2812B_TIM_DMA_CCR     (WS2812B_TIM->CCR1)
+#define WS2812B_TIM             TIM1
+#define WS2812B_TIM_OCINIT      TIM_OC4Init
+#define WS2812B_TIM_OCPRELOAD   TIM_OC4PreloadConfig
+#define WS2812B_TIM_DMA_CC      TIM_DMA_CC4
+#define WS2812B_TIM_DMA_CCR     (WS2812B_TIM->CCR4)
 
 #define WS2812B_DMA             DMA1
-#define WS2812B_DMA_CHANNEL     DMA1_Channel1
-#define WS2812B_DMA_IRQ         DMA1_Channel1_IRQn
+#define WS2812B_DMA_CHANNEL     DMA1_Channel4
+#define WS2812B_DMA_IRQ         DMA1_Channel4_IRQn
 
-#define WS2812B_DMA_HANDLER     DMA1_Channel1_IRQHandler
-#define WS2812B_DMA_IT_TC       DMA1_IT_TC1
-#define WS2812B_DMA_IT_HT       DMA1_IT_HT1
+#define WS2812B_DMA_HANDLER     DMA1_Channel4_IRQHandler
+#define WS2812B_DMA_IT_TC       DMA1_IT_TC4   /// Transfer complete interrupt flag, number is channel
+#define WS2812B_DMA_IT_HT       DMA1_IT_HT4   /// Half-transfer interrupt flag, number is channel
 
 #define WS2812B_IRQ_PRIO        0
 #define WS2812B_IRQ_SUBPRIO     0
